e9c21561d389fec4a22f3917dcf4aaa911864fdf
Roman Pominov <rpominov@gmail.com>
2014-05-11 20:42:01 +0400

    fix repeatedly test (maybe)

--- test/specs/repeatedly.js
+++ test/specs/repeatedly.js
@@ -3,25 +3,25 @@ var helpers = require('../test-helpers');
 
 
 
-// describe("repeatedly:", function(){
+describe("repeatedly:", function(){
 
-//   it("works", function(done){
+  it("works", function(done){
 
-//     var stream1 = helpers.sampleStream([1, Kefir.END]);
-//     var stream2 = Kefir.repeatedly(30, [2, 4]).take(5);
-//     var stream3 = Kefir.repeatedly(45, [3, 5]).take(1);
+    var stream1 = helpers.sampleStream([1, Kefir.END]);
+    var stream2 = Kefir.repeatedly(30, [2, 4]).take(5);
+    var stream3 = Kefir.repeatedly(45, [3, 5]).take(1);
 
-//     // 1
-//     // ---2---4---2---4---2
-//     // -----3
-//     var merged = stream1.merge(stream2, stream3);
+    // 1
+    // ---2---4---2---4---2
+    // -----3
+    var merged = stream1.merge(stream2, stream3);
 
-//     helpers.captureOutput(merged, function(values){
-//       expect(values).toEqual([1, 2, 3, 4, 2, 4, 2]);
-//       done();
-//     });
+    helpers.captureOutput(merged, function(values){
+      expect(values).toEqual([1, 2, 3, 4, 2, 4, 2]);
+      done();
+    });
 
-//   }, 200);
+  }, 200);
 
 
-// });
+});
