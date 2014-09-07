2a81bee6acde2e65ba64bc934b09e91f5a94047f
Roman Pominov <rpominov@gmail.com>
2014-09-07 23:37:29 +0400

    0.2.5

--- test/in-browser/spec/KefirSpecs.js
+++ test/in-browser/spec/KefirSpecs.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-/*! kefir - 0.2.4
+/*! kefir - 0.2.5
  *  https://github.com/pozadi/kefir
  */
 ;(function(global){
@@ -20160,7 +20160,7 @@ beforeEach(function() {
 
 
 },{"../dist/kefir":1,"sinon":7}],58:[function(require,module,exports){
-/*! kefir addon - 0.2.4
+/*! kefir addon - 0.2.5
  *  https://github.com/pozadi/kefir
  */
 ;(function(global){
