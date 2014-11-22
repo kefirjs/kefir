kefirjs/kefir

commit 31bcb19189ac4d047cdef1133ccdd91cceb5c235

remove "array functions" #19

src/core.js
@@ -1,54 +1,55 @@
 // Subscribers
 
 function Subscribers() {
-  this._fns = [];
+  this._items = [];
 }
 
 extend(Subscribers, {
-  callOne: function(fn, event) {
-    if (fn.type === ANY) {
-      fn.invoke(event);
-    } else if (fn.type === event.type) {
-      if (fn.type === VALUE) {
-        fn.invoke(event.value);
+  callOne: function(fnData, event) {
+    if (fnData.type === ANY) {
+      fnData.fn(event);
+    } else if (fnData.type === event.type) {
+      if (fnData.type === VALUE) {
+        fnData.fn(event.value);
       } else {
-        fn.invoke();
+        fnData.fn();
       }
     }
   },
-  callOnce: function(type, fnMeta, event) {
+  callOnce: function(type, fn, event) {
     if (type === ANY) {
-      applyFnMeta(fnMeta, [event]);
+      fn(event);
     } else if (type === event.type) {
       if (type === VALUE) {
-        applyFnMeta(fnMeta, [event.value]);
+        fn(event.value);
       } else {
-        applyFnMeta(fnMeta, []);
+        fn();
       }
     }
   }
 });
 
 extend(Subscribers.prototype, {
-  add: function(type, fn) {
-    fn = Fn(fn, type === END ? 0 : 1);
-    fn.type = type;
-    this._fns = concat(this._fns, [fn]);
+  add: function(type, fn, _key) {
+    this._items = concat(this._items, [{
+      type: type,
+      fn: fn,
+      key: _key || NOTHING
+    }]);
   },
-  remove: function(type, fn) {
-    fn = Fn(fn);
-    this._fns = removeByPred(this._fns, function(x) {
-      return x.type === type && Fn.isEqual(x, fn);
+  remove: function(type, fn, _key) {
+    this._items = removeByPred(this._items, function(fnData) {
+      return fnData.type === type && (fnData.key === _key || fnData.fn === fn);
     });
   },
   callAll: function(event) {
-    var fns = this._fns;
-    for (var i = 0; i < fns.length; i++) {
-      Subscribers.callOne(fns[i], event);
+    var items = this._items;
+    for (var i = 0; i < items.length; i++) {
+      Subscribers.callOne(items[i], event);
     }
   },
   isEmpty: function() {
-    return this._fns.length === 0;
+    return this._items.length === 0;
   }
 });
 
@@ -108,9 +109,9 @@ extend(Observable.prototype, {
     }
   },
 
-  on: function(type, fn) {
+  on: function(type, fn, _key) {
     if (this._alive) {
-      this._subscribers.add(type, fn);
+      this._subscribers.add(type, fn, _key);
       this._setActive(true);
     } else {
       Subscribers.callOnce(type, fn, CURRENT_END);
@@ -118,9 +119,9 @@ extend(Observable.prototype, {
     return this;
   },
 
-  off: function(type, fn) {
+  off: function(type, fn, _key) {
     if (this._alive) {
-      this._subscribers.remove(type, fn);
+      this._subscribers.remove(type, fn, _key);
       if (this._subscribers.isEmpty()) {
         this._setActive(false);
       }
@@ -128,13 +129,13 @@ extend(Observable.prototype, {
     return this;
   },
 
-  onValue:  function(fn) {  return this.on(VALUE, fn)   },
-  onEnd:    function(fn) {  return this.on(END, fn)     },
-  onAny:    function(fn) {  return this.on(ANY, fn)     },
+  onValue:  function(fn, _key) {  return this.on(VALUE, fn, _key)   },
+  onEnd:    function(fn, _key) {  return this.on(END, fn, _key)     },
+  onAny:    function(fn, _key) {  return this.on(ANY, fn, _key)     },
 
-  offValue: function(fn) {  return this.off(VALUE, fn)  },
-  offEnd:   function(fn) {  return this.off(END, fn)    },
-  offAny:   function(fn) {  return this.off(ANY, fn)    }
+  offValue: function(fn, _key) {  return this.off(VALUE, fn, _key)  },
+  offEnd:   function(fn, _key) {  return this.off(END, fn, _key)    },
+  offAny:   function(fn, _key) {  return this.off(ANY, fn, _key)    }
 
 });
 
@@ -191,9 +192,9 @@ inherit(Property, Observable, {
     }
   },
 
-  on: function(type, fn) {
+  on: function(type, fn, _key) {
     if (this._alive) {
-      this._subscribers.add(type, fn);
+      this._subscribers.add(type, fn, _key);
       this._setActive(true);
     }
     if (this._current !== NOTHING) {
@@ -214,21 +215,21 @@ inherit(Property, Observable, {
 
 // Log
 
-function logCb(name, event) {
-  var typeStr = '<' + event.type + (event.current ? ':current' : '') + '>';
-  if (event.type === VALUE) {
-    console.log(name, typeStr, event.value);
-  } else {
-    console.log(name, typeStr);
-  }
-}
-
 Observable.prototype.log = function(name) {
-  this.onAny([logCb, null, name || this.toString()]);
+  name = name || this.toString();
+  this.onAny(function(event) {
+    var typeStr = '<' + event.type + (event.current ? ':current' : '') + '>';
+    if (event.type === VALUE) {
+      console.log(name, typeStr, event.value);
+    } else {
+      console.log(name, typeStr);
+    }
+  }, '__logKey__' + name);
   return this;
 }
 
 Observable.prototype.offLog = function(name) {
-  this.offAny([logCb, null, name || this.toString()]);
+  name = name || this.toString();
+  this.offAny(null, '__logKey__' + name);
   return this;
 }

src/multiple-sources.js
@@ -8,6 +8,9 @@ function _AbstractPool(options) {
     throw new Error('options.concurLim can\'t be 0');
   }
 
+  var $ = this;
+  this._$handleSubAny = function(event) {  $._handleSubAny(event)  };
+
   this._queue = [];
   this._curSources = [];
   this._activating = false;
@@ -48,12 +51,13 @@ inherit(_AbstractPool, Stream, {
     if (this._active) {  this._sub(obs)  }
   },
   _sub: function(obs) {
-    obs.onAny([this._handleSubAny, this]);
-    obs.onEnd([this._removeCur, this, obs]);
+    var $ = this;
+    obs.onAny(this._$handleSubAny);
+    obs.onEnd(function() {  $._removeCur(obs)  }, obs);
   },
   _unsub: function(obs) {
-    obs.offAny([this._handleSubAny, this]);
-    obs.offEnd([this._removeCur, this, obs]);
+    obs.offAny(this._$handleSubAny);
+    obs.offEnd(null, obs);
   },
   _handleSubAny: function(event) {
     if (event.type === VALUE) {
@@ -110,6 +114,7 @@ inherit(_AbstractPool, Stream, {
     Stream.prototype._clear.call(this);
     this._queue = null;
     this._curSources = null;
+    this._$handleSubAny = null;
   }
 
 });
@@ -240,9 +245,12 @@ Kefir.bus = function() {
 function FlatMap(source, fn, options) {
   _AbstractPool.call(this, options);
   this._source = source;
-  this._fn = fn ? buildFn(fn, 1) : id;
+  this._fn = fn || id;
   this._mainEnded = false;
   this._lastCurrent = null;
+
+  var $ = this;
+  this._$handleMainSource = function(event) {  $._handleMainSource(event)  };
 }
 
 inherit(FlatMap, _AbstractPool, {
@@ -250,12 +258,12 @@ inherit(FlatMap, _AbstractPool, {
   _onActivation: function() {
     _AbstractPool.prototype._onActivation.call(this);
     this._activating = true;
-    this._source.onAny([this._handleMainSource, this]);
+    this._source.onAny(this._$handleMainSource);
     this._activating = false;
   },
   _onDeactivation: function() {
     _AbstractPool.prototype._onDeactivation.call(this);
-    this._source.offAny([this._handleMainSource, this]);
+    this._source.offAny(this._$handleMainSource);
   },
 
   _handleMainSource: function(event) {
@@ -281,6 +289,7 @@ inherit(FlatMap, _AbstractPool, {
     _AbstractPool.prototype._clear.call(this);
     this._source = null;
     this._lastCurrent = null;
+    this._$handleMainSource = null;
   }
 
 });
@@ -329,7 +338,7 @@ function SampledBy(passive, active, combinator) {
   } else {
     this._passiveCount = passive.length;
     this._sources = concat(passive, active);
-    this._combinator = combinator ? buildFnSpread(combinator, this._sources.length) : id;
+    this._combinator = combinator ? spread(combinator, this._sources.length) : id;
     this._aliveCount = 0;
     this._currents = new Array(this._sources.length);
     fillArray(this._currents, NOTHING);
@@ -337,6 +346,11 @@ function SampledBy(passive, active, combinator) {
     this._emitAfterActivation = false;
     this._endAfterActivation = false;
   }
+}
+
+
+function bind_SampledBy_handleAny($, i) {
+  return function(event) {  $._handleAny(i, event)  };
 }
 
 inherit(SampledBy, Stream, {
@@ -349,7 +363,7 @@ inherit(SampledBy, Stream, {
     this._aliveCount = length - this._passiveCount;
     this._activating = true;
     for (i = 0; i < length; i++) {
-      this._sources[i].onAny([this._handleAny, this, i]);
+      this._sources[i].onAny(bind_SampledBy_handleAny(this, i), this._sources[i]);
     }
     this._activating = false;
     if (this._emitAfterActivation) {
@@ -365,7 +379,7 @@ inherit(SampledBy, Stream, {
     var length = this._sources.length,
         i;
     for (i = 0; i < length; i++) {
-      this._sources[i].offAny([this._handleAny, this, i]);
+      this._sources[i].offAny(null, this._sources[i]);
     }
   },
 

demos/counter.html
@@ -61,18 +61,18 @@
   var plusClicks = $('#plus-btn').asKefirStream('click');
   var minusClicks = $('#minus-btn').asKefirStream('click');
 
-  var counterChanges = Kefir.merge(
+  var counterChanges = Kefir.merge([
     plusClicks.mapTo(1),
     minusClicks.mapTo(-1)
-  )
+  ])
 
   function sum(a, b) {
     return a + b;
   }
 
-  var counterValue = counterChanges.scan(0, sum);
+  var counterValue = counterChanges.scan(sum, 0);
 
-  counterValue.onValue(['text', $('#result')]);
+  counterValue.onValue(function(n) {  $('#result').text(n)  });
 
 &lt;/script&gt;
 
@@ -86,18 +86,18 @@
         var plusClicks = $('#plus-btn').asKefirStream('click');
         var minusClicks = $('#minus-btn').asKefirStream('click');
 
-        var counterChanges = Kefir.merge(
+        var counterChanges = Kefir.merge([
           plusClicks.mapTo(1),
           minusClicks.mapTo(-1)
-        )
+        ])
 
         function sum(a, b) {
           return a + b;
         }
 
-        var counterValue = counterChanges.scan(0, sum);
+        var counterValue = counterChanges.scan(sum, 0);
 
-        counterValue.onValue(['text', $('#result')]);
+        counterValue.onValue(function(n) {  $('#result').text(n)  });
 
       </script>
 

demos/custom-stream.html
@@ -107,7 +107,7 @@
 
   var clickPositions = clicks.map(getPosition);
 
-  clickPositions.onValue(['css', $('#pos-marker')]);
+  clickPositions.onValue(function(css) {  $('#pos-marker').css(css)  });
 
 &lt;/script&gt;
 
@@ -140,7 +140,7 @@
 
         var clickPositions = clicks.map(getPosition);
 
-        clickPositions.onValue(['css', $('#pos-marker')]);
+        clickPositions.onValue(function(css) {  $('#pos-marker').css(css)  });
 
       </script>
 

demos/position.html
@@ -107,7 +107,7 @@
     };
   });
 
-  pos.onValue(['css', $('#pos-marker')]);
+  pos.onValue(function(css) {  $('#pos-marker').css(css)  });
 
 &lt;/script&gt;
 
@@ -138,7 +138,7 @@
           };
         });
 
-        pos.onValue(['css', $('#pos-marker')]);
+        pos.onValue(function(css) {  $('#pos-marker').css(css)  });
 
       </script>
 

dist/addons/kefir-jquery.js
@@ -15,18 +15,12 @@
         transformer = selector;
         selector = null;
       }
-      transformer = transformer && Kefir.Fn(transformer);
       return Kefir.fromBinder(function(emitter) {
-        var onEvent;
-        if (transformer) {
-          onEvent = function() {
-            emitter.emit(transformer.applyWithContext(this, arguments));
-          };
-        } else {
-          onEvent = emitter.emit;
-        }
+        var onEvent = transformer ?
+          function() {  emitter.emit(transformer.apply(this, arguments))  } :
+          emitter.emit;
         $el.on(eventName, selector, onEvent);
-        return ['off', $el, eventName, selector, onEvent];
+        return function() {  $el.off(eventName, selector, onEvent)  };
       }).setName('asKefirStream');
     }
 
@@ -38,9 +32,8 @@
         getter = selector;
         selector = null;
       }
-      getter = Kefir.Fn(getter);
       return this.asKefirStream(eventName, selector, getter)
-        .toProperty(getter.invoke())
+        .toProperty(getter())
         .setName('asKefirProperty');
     }
 

commit 41009b9d61a7d87328444aad11ed481aec81a9b7

fix a bug introduced in 31bcb19

dist/kefir.js
@@ -546,7 +546,8 @@ extend(Subscribers.prototype, {
   },
   remove: function(type, fn, _key) {
     this._items = removeByPred(this._items, function(fnData) {
-      return fnData.type === type && (fnData.key === _key || fnData.fn === fn);
+      return fnData.type === type &&
+        (fnData.fn === fn || isEqualArrays(fnData.key, _key));
     });
   },
   callAll: function(event) {
@@ -918,11 +919,11 @@ inherit(_AbstractPool, Stream, {
   _sub: function(obs) {
     var $ = this;
     obs.onAny(this._$handleSubAny);
-    obs.onEnd(function() {  $._removeCur(obs)  }, obs);
+    obs.onEnd(function() {  $._removeCur(obs)  }, [this, obs]);
   },
   _unsub: function(obs) {
     obs.offAny(this._$handleSubAny);
-    obs.offEnd(null, obs);
+    obs.offEnd(null, [this, obs]);
   },
   _handleSubAny: function(event) {
     if (event.type === VALUE) {
@@ -1228,7 +1229,7 @@ inherit(SampledBy, Stream, {
     this._aliveCount = length - this._passiveCount;
     this._activating = true;
     for (i = 0; i < length; i++) {
-      this._sources[i].onAny(bind_SampledBy_handleAny(this, i), this._sources[i]);
+      this._sources[i].onAny(bind_SampledBy_handleAny(this, i), [this, i]);
     }
     this._activating = false;
     if (this._emitAfterActivation) {
@@ -1244,7 +1245,7 @@ inherit(SampledBy, Stream, {
     var length = this._sources.length,
         i;
     for (i = 0; i < length; i++) {
-      this._sources[i].offAny(null, this._sources[i]);
+      this._sources[i].offAny(null, [this, i]);
     }
   },
 

src/core.js
@@ -39,7 +39,8 @@ extend(Subscribers.prototype, {
   },
   remove: function(type, fn, _key) {
     this._items = removeByPred(this._items, function(fnData) {
-      return fnData.type === type && (fnData.key === _key || fnData.fn === fn);
+      return fnData.type === type &&
+        (fnData.fn === fn || isEqualArrays(fnData.key, _key));
     });
   },
   callAll: function(event) {

src/multiple-sources.js
@@ -53,11 +53,11 @@ inherit(_AbstractPool, Stream, {
   _sub: function(obs) {
     var $ = this;
     obs.onAny(this._$handleSubAny);
-    obs.onEnd(function() {  $._removeCur(obs)  }, obs);
+    obs.onEnd(function() {  $._removeCur(obs)  }, [this, obs]);
   },
   _unsub: function(obs) {
     obs.offAny(this._$handleSubAny);
-    obs.offEnd(null, obs);
+    obs.offEnd(null, [this, obs]);
   },
   _handleSubAny: function(event) {
     if (event.type === VALUE) {
@@ -363,7 +363,7 @@ inherit(SampledBy, Stream, {
     this._aliveCount = length - this._passiveCount;
     this._activating = true;
     for (i = 0; i < length; i++) {
-      this._sources[i].onAny(bind_SampledBy_handleAny(this, i), this._sources[i]);
+      this._sources[i].onAny(bind_SampledBy_handleAny(this, i), [this, i]);
     }
     this._activating = false;
     if (this._emitAfterActivation) {
@@ -379,7 +379,7 @@ inherit(SampledBy, Stream, {
     var length = this._sources.length,
         i;
     for (i = 0; i < length; i++) {
-      this._sources[i].offAny(null, this._sources[i]);
+      this._sources[i].offAny(null, [this, i]);
     }
   },
 

test/in-browser/spec/KefirSpecs.js
@@ -547,7 +547,8 @@ extend(Subscribers.prototype, {
   },
   remove: function(type, fn, _key) {
     this._items = removeByPred(this._items, function(fnData) {
-      return fnData.type === type && (fnData.key === _key || fnData.fn === fn);
+      return fnData.type === type &&
+        (fnData.fn === fn || isEqualArrays(fnData.key, _key));
     });
   },
   callAll: function(event) {
@@ -919,11 +920,11 @@ inherit(_AbstractPool, Stream, {
   _sub: function(obs) {
     var $ = this;
     obs.onAny(this._$handleSubAny);
-    obs.onEnd(function() {  $._removeCur(obs)  }, obs);
+    obs.onEnd(function() {  $._removeCur(obs)  }, [this, obs]);
   },
   _unsub: function(obs) {
     obs.offAny(this._$handleSubAny);
-    obs.offEnd(null, obs);
+    obs.offEnd(null, [this, obs]);
   },
   _handleSubAny: function(event) {
     if (event.type === VALUE) {
@@ -1229,7 +1230,7 @@ inherit(SampledBy, Stream, {
     this._aliveCount = length - this._passiveCount;
     this._activating = true;
     for (i = 0; i < length; i++) {
-      this._sources[i].onAny(bind_SampledBy_handleAny(this, i), this._sources[i]);
+      this._sources[i].onAny(bind_SampledBy_handleAny(this, i), [this, i]);
     }
     this._activating = false;
     if (this._emitAfterActivation) {
@@ -1245,7 +1246,7 @@ inherit(SampledBy, Stream, {
     var length = this._sources.length,
         i;
     for (i = 0; i < length; i++) {
-      this._sources[i].offAny(null, this._sources[i]);
+      this._sources[i].offAny(null, [this, i]);
     }
   },
 
@@ -24893,7 +24894,7 @@ describe('sampledBy', function() {
       return send(b, [5, 6, '<end>']);
     });
   });
-  return it('when activating second time and has 2+ properties in sources, should emit current value at most once', function() {
+  it('when activating second time and has 2+ properties in sources, should emit current value at most once', function() {
     var a, b, c, sb;
     a = send(prop(), [0]);
     b = send(prop(), [1]);
@@ -24906,6 +24907,19 @@ describe('sampledBy', function() {
         current: [0, 1, 2]
       }
     ]);
+  });
+  return it('one sampledBy should remove listeners of another', function() {
+    var a, b, s1, s2;
+    a = send(prop(), [0]);
+    b = stream();
+    s1 = a.sampledBy(b);
+    s2 = a.sampledBy(b);
+    activate(s1);
+    activate(s2);
+    deactivate(s2);
+    return expect(s1).toEmit([0], function() {
+      return send(b, [1]);
+    });
   });
 });
 

test/specs/sampled-by.coffee
@@ -89,3 +89,14 @@ describe 'sampledBy', ->
     activate(sb)
     deactivate(sb)
     expect(sb).toEmit [{current: [0, 1, 2]}]
+
+  it 'one sampledBy should remove listeners of another', ->
+    a = send(prop(), [0])
+    b = stream()
+    s1 = a.sampledBy(b)
+    s2 = a.sampledBy(b)
+    activate(s1)
+    activate(s2)
+    deactivate(s2)
+    expect(s1).toEmit [0], ->
+      send(b, [1])