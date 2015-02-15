fb760fdc50d30c7a1c957ef533688365dddf2fb1
Roman Pominov <rpominov@gmail.com>
2015-02-15 14:27:11 +0300

    add Kefir.repeat() method

--- src/primary.js
+++ src/primary.js
@@ -135,3 +135,80 @@ Kefir.constantError = function(x) {
   return new ConstantError(x);
 }
 
+
+
+
+// Kefir.repeat(generator)
+
+function Repeat(generator) {
+  Stream.call(this);
+  this._generator = generator;
+  this._source = null;
+  this._inLoop = false;
+  this._activating = false;
+  this._iteration = 0;
+
+  var $ = this;
+  this._$handleAny = function(event) {
+    $._handleAny(event);
+  };
+}
+
+inherit(Repeat, Stream, {
+
+  _name: 'repeat',
+
+  _handleAny: function(event) {
+    if (event.type === END) {
+      this._source = null;
+      this._startLoop();
+    } else {
+      this._send(event.type, event.value, this._activating);
+    }
+  },
+
+  _startLoop: function() {
+    if (!this._inLoop) {
+      this._inLoop = true;
+      while (this._source === null && this._alive && this._active) {
+        this._source = this._generator(this._iteration++);
+        if (this._source) {
+          this._source.onAny(this._$handleAny);
+        } else {
+          this._send(END);
+        }
+      }
+      this._inLoop = false;
+    }
+  },
+
+  _onActivation: function() {
+    this._activating = true;
+    if (this._source) {
+      this._source.onAny(this._$handleAny);
+    } else {
+      this._startLoop();
+    }
+    this._activating = false;
+  },
+
+  _onDeactivation: function() {
+    if (this._source) {
+      this._source.offAny(this._$handleAny);
+    }
+  },
+
+  _clear: function() {
+    Stream.prototype._clear.call(this);
+    this._generator = null;
+    this._source = null;
+    this._$handleAny = null;
+  }
+
+});
+
+Kefir.repeat = function(generator) {
+  return new Repeat(generator);
+}
+
+
