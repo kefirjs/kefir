5f7f8950e7883bd74c9a0b445dbe678c1d8ca8e9
Roman Pominov <rpominov@gmail.com>
2015-05-03 16:38:44 +0300

    simplify dispatcher

--- src/dispatcher.js
+++ src/dispatcher.js
@@ -2,14 +2,14 @@ const {extend} = require('./utils/objects');
 const {VALUE, ERROR, ANY} = require('./constants');
 const {concat, removeByPred} = require('./utils/collections');
 
-function callSubscriber(sType, sFn, event) {
-  if (sType === ANY) {
-    sFn(event);
-  } else if (sType === event.type) {
-    if (sType === VALUE || sType === ERROR) {
-      sFn(event.value);
+function callSubscriber(type, fn, event) {
+  if (type === ANY) {
+    fn(event);
+  } else if (type === event.type) {
+    if (type === VALUE || type === ERROR) {
+      fn(event.value);
     } else {
-      sFn();
+      fn();
     }
   }
 }
@@ -19,25 +19,23 @@ function Dispatcher() {
 }
 
 extend(Dispatcher.prototype, {
+
   add(type, fn) {
-    this._items = concat(this._items, [{
-      type: type,
-      fn: fn
-    }]);
+    this._items = concat(this._items, [{type, fn}]);
     return this._items.length;
   },
+
   remove(type, fn) {
-    this._items = removeByPred(this._items, function(fnData) {
-      return fnData.type === type && fnData.fn === fn;
-    });
+    this._items = removeByPred(this._items, (x) => x.type === type && x.fn === fn);
     return this._items.length;
   },
+
   dispatch(event) {
-    let items = this._items;
-    for (let i = 0; i < items.length; i++) {
+    for (let i = 0, items = this._items; i < items.length; i++) {
       callSubscriber(items[i].type, items[i].fn, event);
     }
   }
+
 });
 
 
