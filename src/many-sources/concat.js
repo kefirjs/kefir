5fbc414b837debad02f2ced9b9a399ba2436511f
Roman Pominov <rpominov@gmail.com>
2015-05-05 23:30:27 +0300

    fix .toString for .concat

--- src/many-sources/concat.js
+++ src/many-sources/concat.js
@@ -3,5 +3,5 @@ const repeat = require('./repeat');
 module.exports = function concat(observables) {
   return repeat(function(index) {
     return observables.length > index ? observables[index] : false;
-  });
+  }).setName('concat');
 }
