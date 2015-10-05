4684a8b2989dad77623b467d5abe0c676f695f74
Roman Pominov <rpominov@gmail.com>
2015-10-05 18:09:28 +0300

    fix bug in .fromEvents when callback may return not undefined
    (fixes #159)

--- src/primary/from-sub-unsub.js
+++ src/primary/from-sub-unsub.js
@@ -4,9 +4,13 @@ const {apply} = require('../utils/functions');
 module.exports = function fromSubUnsub(sub, unsub, transformer /* Function | falsey */) {
   return stream(function(emitter) {
 
-    let handler = transformer ? function() {
-      emitter.emit(apply(transformer, this, arguments));
-    } : emitter.emit;
+    let handler = transformer
+      ? function() {
+        emitter.emit(apply(transformer, this, arguments));
+      }
+      : x => {
+        emitter.emit(x)
+      };
 
     sub(handler);
     return () => unsub(handler);
