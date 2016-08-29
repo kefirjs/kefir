6c249c908f08f7dcc83c429cb83e4a561a41961e
Chris Cowan <agentme49@gmail.com>
2016-08-29 02:52:16 -0700

    flow: .filter() and .filter(Boolean) removes falsey values from stream.

---
+++ test/flow/filter.js
@@ -0,0 +1,48 @@
+/* @flow */
+
+import Kefir from '../../kefir';
+
+Kefir.sequentially(10, [5, null])
+  .onValue(x => {
+    const good: ?number = x;
+    // $ExpectError
+    const bad1: number = x;
+    // $ExpectError
+    const bad2: null = x;
+  })
+  .filter()
+  .onValue(x => {
+    const good: number = x;
+    // $ExpectError
+    const bad: null = x;
+  });
+
+Kefir.sequentially(10, [5, null])
+  .onValue(x => {
+    const good: ?number = x;
+    // $ExpectError
+    const bad1: number = x;
+    // $ExpectError
+    const bad2: null = x;
+  })
+  .filter(Boolean)
+  .onValue(x => {
+    const good: number = x;
+    // $ExpectError
+    const bad: null = x;
+  });
+
+Kefir.sequentially(10, [5, null])
+  .onValue(x => {
+    const good: ?number = x;
+    // $ExpectError
+    const bad1: number = x;
+    // $ExpectError
+    const bad2: null = x;
+  })
+  .filter(x => true)
+  .onValue(x => {
+    const good: ?number = x;
+    // $ExpectError
+    const bad: number = x;
+  });
