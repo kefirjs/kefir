fd9ae4ca026549713bdaf518074a98d56ef4b2ad
Chris Cowan <agentme49@gmail.com>
2016-08-29 02:41:02 -0700

    Add Flow type declarations (#217)
    * Add flow type declaration file.
    
    * Remove extra _Emitter class.
    
    * Add .flowconfig.
    
    * Add flow-bin dev dependency.
    
    * V type parameter instead of T to represent value.
    
    * Parameterize the error type.
    
    * Add some flow declaration tests.
    
    * Finish staticLand types.
    
    * Streams and Properties are covariant.
    
    * Copy kefir.js.flow into dist/ on build.
    
    * Observer methods are optional.
    
    * toProperty tweak.
    
    * Fix staticLand concat type.
    
    * Remove some redundant optional parameters where a matching overload already exists.
    
    * Tweak fromPromise type.
    
    * readme flow note
    
    * Support combinator as second parameter to Kefir.combine.

---
+++ test/flow/error.js
@@ -0,0 +1,21 @@
+/* @flow */
+
+import Kefir from '../../kefir';
+
+// error type should be persisted through and unaffected by map call.
+const prop = Kefir.constantError(1)
+  .merge(Kefir.constant(2))
+  .map(x => String(x))
+  .mapErrors(err => ({a: err}));
+
+prop
+  .onValue(x => {
+    const good: string = x;
+    // $ExpectError
+    const bad: number = x;
+  })
+  .onError(x => {
+    const good: {a: number} = x;
+    // $ExpectError
+    const bad: string = x;
+  });
---
+++ test/flow/propType.js
@@ -0,0 +1,11 @@
+/* @flow */
+
+import Kefir from '../../kefir';
+
+const prop = Kefir.constant(1);
+
+prop.onValue(x => {
+  const n: number = x;
+  // $ExpectError
+  const s: string = x;
+});
