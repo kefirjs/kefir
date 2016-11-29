5f08937bb37830b12608662657c55275cf48be85
Chris Cowan <agentme49@gmail.com>
2016-11-28 16:04:19 -0800

    Update Flow dev dep, make changes to keep compatibility.

---
+++ test/flow/observe.js
@@ -0,0 +1,29 @@
+/* @flow */
+
+import Kefir from '../../kefir';
+
+const s1: Kefir.Observable<number> = Kefir.constant(1);
+
+s1.observe({
+  value(x) {
+    const good: number = x;
+    // $ExpectError
+    const bad: string = x;
+  },
+  error: (e) => {
+
+  }
+});
+
+class MyObserver {
+  value(x) {
+    const good: number = x;
+    // $ExpectError
+    const bad: string = x;
+  }
+  error(e) {
+
+  }
+}
+
+s1.observe(new MyObserver());
