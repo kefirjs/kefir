2fdff0019ec6d69fa8bde6f43ff5308851bcd270
Roman Pominov <rpominov@gmail.com>
2017-10-22 13:54:53 +0300

    use bash explicitly in prettier.sh

--- configs/prettier.sh
+++ configs/prettier.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 PRETTIER="`npm bin`/prettier --no-semi --no-bracket-spacing --print-width 120 --single-quote --trailing-comma es5"
 
-if test "$1" == "check"; then
+if [ "$1" == "check" ]; then
   echo "Running prettier check. Files that should be formatted (shouldn't be any):"
   $PRETTIER --list-different '{src,configs,test}/**/*.js' '*.js'
 else
